61dadd0088bd61e9af5665c0986d3068a17e6fe9
Gherkin <niyohn@stekligan.se>
2015-10-07 11:37:21 +0200

    switched to MySQL

--- src/main/java/com/github/gherkin/entity/ClubEntity.java
+++ src/main/java/com/github/gherkin/entity/ClubEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "CLUB", schema = "APP")
+@Table(name = "CLUB")
 public class ClubEntity {
 
 	@Id
--- src/main/java/com/github/gherkin/entity/PersonEntity.java
+++ src/main/java/com/github/gherkin/entity/PersonEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "PERSON", schema = "APP")
+@Table(name = "PERSON")
 public class PersonEntity {
 
 	@Id
