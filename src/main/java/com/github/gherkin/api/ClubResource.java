82b821a011287303cb8eaa828bbab13188f6f01b
Gherkin <niyohn@stekligan.se>
2015-10-08 10:54:26 +0200

    cleaned imports

--- src/main/java/com/github/gherkin/api/ClubResource.java
+++ src/main/java/com/github/gherkin/api/ClubResource.java
@@ -4,7 +4,14 @@ import java.util.Collection;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.MediaType;
 
 import com.github.gherkin.api.data.Club;
